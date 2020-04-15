d5974e22150fdbbf8b3171ac756ab145e27fa6d9
yuntian <944610721@qq.com>
2020-04-15 23:48:01 +0800

    添加消息fastjson消息体

--- mybatis-dynamic-data/src/main/java/com/yuntian/mybatisdynamic/MybatisDynamicDemoApplication.java
+++ mybatis-dynamic-data/src/main/java/com/yuntian/mybatisdynamic/MybatisDynamicDemoApplication.java
@@ -5,6 +5,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Administrator
+ */
 @MapperScan("com.yuntian.mybatisdynamic.dao")
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class MybatisDynamicDemoApplication {
